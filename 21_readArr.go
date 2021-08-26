a8cea17ddc9d0b8ebba356a142fbffd999aed546
Elizabeth <mphilz@mail.ru>
2021-08-26 10:03:02 +0300

    new version

--- 21_readArr.go
+++ 21_readArr.go
@@ -6,14 +6,23 @@ import (
 )
 
 func main() {
-	var wg sync.WaitGroup
+	// Создаем счетчик горутин
+	wg := new(sync.WaitGroup)
+
+	// Задаем исходные данные
 	arr := [7]int{1, 2, 3, 4, 5, 6, 7}
-	wg.Add(len(arr))
-	go func() {
-		for _, i := range arr {
-			fmt.Println(i)
-			wg.Done()
-		}
-	}()
+
+	// Вызываем функцию вывода через итерацию по элементам массива
+	for _, i := range arr {
+		wg.Add(1)
+		go print(i, wg)
+	}
 	wg.Wait()
+}
+
+// Функция выводит значение в консоль
+func print(i int, wg *sync.WaitGroup) {
+	fmt.Println(i)
+	wg.Done() // Убавляем счетчик на 1 после вывода
+
 }
--- 2_sq.go
+++ 2_sq.go
@@ -1,39 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
+	// Создаем счетчик горутин
+	wg := new(sync.WaitGroup)
+
 	// Задаем исходные данные
-	m := []int{2, 4, 6, 8, 10, 12}
-	// Выводим в консоль, пройдясь циклом
-	for i := range sq(sliceChan(m)) {
-		fmt.Println(i)
+	arr := [5]int{2, 4, 6, 8, 10}
+
+	// Вызываем функцию вывода через итерацию по элементам массива
+	for _, i := range arr {
+		wg.Add(1) //Прибавляем счетчик на 1
+		go printSq(i, wg)
 	}
+	wg.Wait()
 }
 
-// Превращаем слайс исходных данных в канал
-func sliceChan(arr []int) <-chan int {
-	firstChan := make(chan int)
-	go func() {
-		for _, i := range arr {
-			firstChan <- i
-		}
-		// Закрываем канал, когда все числа были отправлены
-		close(firstChan)
-	}()
-	return firstChan
-}
-
-// Читаем числа из канала с исходными данными
-// Записываем в новый канал квадрат каждого прочитанного числа
-func sq(ch <-chan int) <-chan int {
-	outChan := make(chan int)
-	go func() {
-		for i := range ch {
-			outChan <- i * i
-		}
-		// Закрываем канал, когда все квадраты чисел были отправлены
-		close(outChan)
-	}()
-	return outChan
+// Функция выводит значение в консоль
+func printSq(i int, wg *sync.WaitGroup) int {
+	defer wg.Done()
+	res := i * i
+	fmt.Println(res)
+	// wg.Done() // Убавляем счетчик на 1 после вывода
+	return res
 }
